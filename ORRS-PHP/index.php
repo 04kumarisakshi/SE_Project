c97a20d7b5084f5322df962f9a56ee390d168d9d
kumari sakshi <sakshi.kumari80102@gmail.com>
2023-11-12 23:08:41 +0530

    Software Prototype

---
+++ ORRS-PHP/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE HTML>
+<!--
+	Online Railway Reservation System
+	martdevelopers254 | @MartinMbithi
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Online Railway Reservation System</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="includes/css/main.css" />
+		<noscript><link rel="stylesheet" href="includes/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload" >
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="index.php">Online Railway Reservation System</a></h1>
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Menu</h2>
+							<ul class="links">
+								<li><a href="index.php">Home</a></li>
+								<li><a href="pass-login.php">User Login</a></li>
+								
+								<li><a href="admin/emp-login.php">Admin Login</a></li>
+							</ul>
+							<a href="#" class="close">Close</a>
+						</div>
+					</nav>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<div class="logo"><span class="icon solid fa-train"></span></div>
+							<h2>Online Commodity Railway Reservation System</h2>
+							<p>Best, Simple, and User Friendly Way To Reserve Train Tickets Effectively</a></p>
+						</div>
+					</section>
+
+					<section id="footer">
+						<div class="inner">
+							<h4>Developed By : Kumari Sakshi</h4>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="includes/js/jquery.min.js"></script>
+			<script src="includes/js/jquery.scrollex.min.js"></script>
+			<script src="includes/js/browser.min.js"></script>
+			<script src="includes/js/breakpoints.min.js"></script>
+			<script src="includes/js/util.js"></script>
+			<script src="includes/js/main.js"></script>
+
+	</body>
+</html>
